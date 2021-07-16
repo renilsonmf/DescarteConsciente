002e193541c2e8e0a82735cd187916543bcedd6f
Renilson <renilson.moreiraf@gmail.com>
2021-07-16 09:37:46 -0300

    Implementando o scrollView

--- DescarteConsciente/Sources/Controller/Login/LoginView.swift
+++ DescarteConsciente/Sources/Controller/Login/LoginView.swift
@@ -10,6 +10,8 @@ import UIKit
 class LoginView: UIView {
     
     var onAccessType: ((_ accessType: AccessType) -> Void)?
+    let scrollView = UIScrollView(frame: .zero)
+    let contentview = UIView(frame: .zero)
     
     //MARK: Inicializadores
     override init(frame: CGRect) {
@@ -22,6 +24,8 @@ class LoginView: UIView {
     }
     func createView() {
         self.backgroundColor = .white
+        configContentView()
+        configScroll()
         setImageIconRecycle()
         setLabelEmail()
         setTextFieldEmail()
@@ -29,25 +33,52 @@ class LoginView: UIView {
         setTextFieldPassword()
         setButtonForgotPassword()
         setButtonLogin()
+        setBottomScroll()
+    }
+    
+    //MARK: Content View
+    private func configContentView() {
+        scrollView.addSubview(contentview)
+        contentview.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentview.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentview.topAnchor.constraint(equalTo: scrollView.topAnchor),
+        ])
+        
+    }
+    //MARK: Scroll View
+    private func configScroll() {
+        self.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.widthAnchor.constraint(equalTo: self.widthAnchor),
+            scrollView.topAnchor.constraint(equalTo: self.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
+    }
+    
+    func setBottomScroll() { //Esse procedimento nao funcionará se o button (que eh o ultimo elemento
+        // e eh o referenciado) nao estiver adicionado e posicionado, por isso ele esta aqui
+        scrollView.bottomAnchor.constraint(equalTo: buttonLogin.bottomAnchor, constant: 300).isActive = true
     }
     
     //MARK: Icon
     lazy var imageIconRecycle: UIImageView = {ImageComponent(setImage: .iconRecycle)}()
     private func setImageIconRecycle() {
                 
-        addSubview(imageIconRecycle)
+        contentview.addSubview(imageIconRecycle)
         NSLayoutConstraint.activate([
-            imageIconRecycle.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 70),
-            imageIconRecycle.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor)
+            imageIconRecycle.topAnchor.constraint(equalTo: contentview.safeAreaLayoutGuide.topAnchor, constant: 70),
+            imageIconRecycle.centerXAnchor.constraint(equalTo: contentview.safeAreaLayoutGuide.centerXAnchor)
         ])
     }
     //MARK: Label Email
     lazy var labelEmail: UILabel = {LabelComponent("Email", .colorMainGreen)}()
     private func setLabelEmail() {
-        addSubview(labelEmail)
+        contentview.addSubview(labelEmail)
         NSLayoutConstraint.activate([
             labelEmail.topAnchor.constraint(equalTo: self.imageIconRecycle.bottomAnchor, constant: 40),
-            labelEmail.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 30),
+            labelEmail.leftAnchor.constraint(equalTo: contentview.leftAnchor, constant: 30),
         ])
     }
     
@@ -62,11 +93,11 @@ class LoginView: UIView {
         return myTextField
     }()
     private func setTextFieldEmail() {
-        addSubview(textFieldEmail)
+        contentview.addSubview(textFieldEmail)
         NSLayoutConstraint.activate([
             textFieldEmail.topAnchor.constraint(equalTo: labelEmail.bottomAnchor, constant: 1),
-            textFieldEmail.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 30),
-            textFieldEmail.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -30),
+            textFieldEmail.leftAnchor.constraint(equalTo: contentview.leftAnchor, constant: 30),
+            textFieldEmail.rightAnchor.constraint(equalTo: contentview.rightAnchor, constant: -30),
             textFieldEmail.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
@@ -74,10 +105,10 @@ class LoginView: UIView {
     //MARK: Label Password
     lazy var labelPassword: UILabel = {LabelComponent("Senha", .colorMainGreen)}()
     private func setLabelPassword() {
-        addSubview(labelPassword)
+        contentview.addSubview(labelPassword)
         NSLayoutConstraint.activate([
             labelPassword.topAnchor.constraint(equalTo: self.textFieldEmail.bottomAnchor, constant: 20),
-            labelPassword.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 30),
+            labelPassword.leftAnchor.constraint(equalTo: contentview.leftAnchor, constant: 30),
         ])
     }
     
@@ -92,11 +123,11 @@ class LoginView: UIView {
         return myTextField
     }()
     private func setTextFieldPassword() {
-        addSubview(textFieldPassword)
+        contentview.addSubview(textFieldPassword)
         NSLayoutConstraint.activate([
             textFieldPassword.topAnchor.constraint(equalTo: labelPassword.bottomAnchor, constant: 1),
-            textFieldPassword.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 30),
-            textFieldPassword.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -30),
+            textFieldPassword.leftAnchor.constraint(equalTo: contentview.leftAnchor, constant: 30),
+            textFieldPassword.rightAnchor.constraint(equalTo: contentview.rightAnchor, constant: -30),
             textFieldPassword.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
@@ -104,11 +135,11 @@ class LoginView: UIView {
     //MARK: Button Forgot Password
     lazy var buttonForgotPassword: UIButton = {ButtonComponent(title: "Esqueci minha senha", colorTitle: .colorMainGreen, fontText: .fontForgotPassword)}()
     private func setButtonForgotPassword() {
-        addSubview(buttonForgotPassword)
+        contentview.addSubview(buttonForgotPassword)
         buttonForgotPassword.addTarget(self, action: #selector(actionButtonForgotPassword), for: .touchUpInside)
         NSLayoutConstraint.activate([
             buttonForgotPassword.topAnchor.constraint(equalTo: self.textFieldPassword.bottomAnchor, constant: 20),
-            buttonForgotPassword.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -30),
+            buttonForgotPassword.rightAnchor.constraint(equalTo: contentview.rightAnchor, constant: -30),
         ])
     }
     
@@ -120,12 +151,12 @@ class LoginView: UIView {
     //MARK: Button Login
     lazy var buttonLogin: UIButton = {ButtonComponent(title: "LOGIN", background: .colorMainGreen, colorTitle: .white, fontText: .fontSubtitleAvenir, radius: 20)}()
     private func setButtonLogin() {
-        addSubview(buttonLogin)
+        contentview.addSubview(buttonLogin)
         buttonLogin.addTarget(self, action: #selector(actionButtonLogin), for: .touchUpInside)
         NSLayoutConstraint.activate([
             buttonLogin.topAnchor.constraint(equalTo: self.buttonForgotPassword.bottomAnchor, constant: 20),
-            buttonLogin.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -30),
-            buttonLogin.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 30),
+            buttonLogin.rightAnchor.constraint(equalTo: contentview.rightAnchor, constant: -30),
+            buttonLogin.leftAnchor.constraint(equalTo: contentview.leftAnchor, constant: 30),
             buttonLogin.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
